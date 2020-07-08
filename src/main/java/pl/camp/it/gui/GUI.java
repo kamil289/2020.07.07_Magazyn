9d5d9784307e8af0f4a247b6651d3543c0241ac8
kamil289 <kamilmusial1988@gmail.com>
2020-07-08 15:57:54 +0200

    nowy

---
+++ src/main/java/pl/camp/it/gui/GUI.java
@@ -0,0 +1,89 @@
+package pl.camp.it.gui;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.Scanner;
+
+public class GUI {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void showMainMenu() {
+        System.out.println("1. Kategorie Produktów");
+        System.out.println("2. Produkty");
+        System.out.println("3. Exit");
+
+        String choose = scanner.nextLine();
+
+        switch (choose) {
+            case "1":
+                showCars();
+                break;
+            case "2":
+                rentCar();
+                break;
+            case "3":
+
+                SQLDb.closeConnection();
+                System.exit(0);
+            default:
+                System.out.println("Nieprawidłowy wybór !!");
+                showMainMenu();
+                break;
+        }
+    }
+
+    private static void showCars() {
+        for(Vehicle tempVehicle : VehicleRepository.getRepository().getVehicles()) {
+            if(tempVehicle != null && !tempVehicle.isRent()) {
+                System.out.println(tempVehicle);
+            }
+        }
+        showMainMenu();
+    }
+
+    private static void rentCar() {
+        System.out.println("Wpisz id samochodu:");
+        String carId = scanner.nextLine();
+        for(Vehicle tempVehicle : VehicleRepository.getRepository().getVehicles()) {
+            try {
+                if (tempVehicle != null && tempVehicle.getId() == Integer.parseInt(carId)) {
+                    if (!tempVehicle.isRent()) {
+                        tempVehicle.setRent(true);
+                        SQLDb.updateVehicleRent(tempVehicle);
+                        System.out.println("Udało się !!");
+                    } else {
+                        System.out.println("Auto niedostępne !!");
+                    }
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Wprowadzono błędne id !!");
+                rentCar();
+            }
+        }
+        showMainMenu();
+    }
+
+    public static void showLoginScreen() {
+        System.out.println("Podaj login:");
+        String login = scanner.nextLine();
+        System.out.println("Podaj hasło:");
+        String password = scanner.nextLine();
+
+        User userFromDataBase = SQLDb.getUserByLogin2(login);
+
+        if(userFromDataBase == null) {
+            showLoginScreen();
+        } else {
+            String hashedPassword = DigestUtils.md5Hex(password);
+
+            if(hashedPassword.equals(userFromDataBase.getPassword())) {
+                showMainMenu();
+            } else {
+                showLoginScreen();
+            }
+        }
+    }
+}
+
+}
